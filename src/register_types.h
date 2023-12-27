d9d39cba371404fa03a0969bc7a7aa6a8af39e19
BleedingXiko <66162192+BleedingXiko@users.noreply.github.com>
2023-12-27 10:56:30 -0800

    Added QTable
    now just need to do the harder parts

--- src/register_types.h
+++ src/register_types.h
@@ -1,11 +1,23 @@
+<<<<<<< Updated upstream
 #ifndef MATRIX_REGISTER_TYPES_H
 #define MATRIX_REGISTER_TYPES_H
+=======
+#ifndef NEURAL_REGISTER_TYPES_H
+#define NEURAL_REGISTER_TYPES_H
+>>>>>>> Stashed changes
 
 #include <godot_cpp/core/class_db.hpp>
 
 using namespace godot;
 
+<<<<<<< Updated upstream
 void initialize_matrix_module(ModuleInitializationLevel p_level);
 void uninitialize_matrix_module(ModuleInitializationLevel p_level);
 
 #endif // MATRIX_REGISTER_TYPES_H
+=======
+void initialize_neural_module(ModuleInitializationLevel p_level);
+void uninitialize_neural_module(ModuleInitializationLevel p_level);
+
+#endif // NEURAL_REGISTER_TYPES_H
+>>>>>>> Stashed changes
